69714bf74609ba44df4a822cea68ac29e4662b4f
simonorlovsky <simon.orlovsky@yahoo.com>
2014-10-11 15:26:31 -0500

    Changes

--- webapp.py
+++ webapp.py
@@ -4,36 +4,40 @@
 
     This is the server-side script of a very simple web application
     that involves an HTML form with two <input type="text"> elements
-    (one named "animal" and the other named "badanimal").
+    (one named "lastname" and the other named "ethnicity").
 '''
 
 import cgi
 
 # Get the user input from the client's request.
 form = cgi.FieldStorage()
-animal = form['animal'].value
-badAnimal = form['badanimal'].value
+lastname = form['lastname'].value
+ethnicity = form['ethnicity'].value
+question = form['question'].value
 
 # Sanitize the user input. You can't trust users not to mess with
-# your scripts. In this case, we allow only letters in our animal
-# names. Otherwise, you're stuck with the default animal.
-if not animal.isalpha():
-    animal = 'DEFAULT ANIMAL'
-if not badAnimal.isalpha():
-    badAnimal = 'DEFAULT ANIMAL'
+# your scripts. In this case, we allow only letters in our lastname
+# names. Otherwise, you're stuck with the default lastname.
+if not lastname.isalpha():
+    lastname = 'DEFAULT lastname'
+if not ethnicity.isalpha():
+    ethnicity = 'DEFAULT ethnicity'
+if not ethnicity.isalpha():
+    question = 'DEFAULT question'
 
 # Construct the output page.
 output = '''<DOCTYPE HTML>
 <html>
 <head>
-    <title>Tiny web app results</title>
+    <title>#NameGame Results</title>
 </head>
 
 <body>
-    <p>I like %ss, too.</p>
-    <p>Also, %ss are gross.</p>
+    <p>I like %s.</p>
+    <p>Also, %s is a cool ethnicity.</p>
+    <p>Hmm, %s good question.</p>
 </body>
-</html>''' % (animal, badAnimal)
+</html>''' % (lastname, ethnicity, question)
 
 # Send the output page back to the client.
 print "Content-type: text/html\r\n\r\n"
